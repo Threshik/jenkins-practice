e667922bc15697278fd0164d425a86d7de1e297e
Threshika V S <115970544+Threshik@users.noreply.github.com>
2025-08-28 22:25:20 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World to the all the people!" );
+        System.out.println( "Hello World !" );
     }
 }
